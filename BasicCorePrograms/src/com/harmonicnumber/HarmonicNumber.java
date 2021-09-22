ae47ca9ea3c6690cf054be70bf14e340402e033d
Kavyashri31 <kavya.karkera31@gmail.com>
2021-09-22 20:55:41 +0530

    HarmonicNumber

--- BasicCorePrograms/src/com/harmonicnumber/HarmonicNumber.java
+++ BasicCorePrograms/src/com/harmonicnumber/HarmonicNumber.java
@@ -1,10 +1,14 @@
 package com.harmonicnumber;
 
+import com.utility.Utility;
+
 public class HarmonicNumber {
 
 	public static void main(String[] args) {
-		System.out.println("Harmoni Number");
-
+		Utility utility = new Utility();
+		System.out.println("Enter the number to check the harmonic number:");
+		int number = utility.getIntValue();
+		utility.getHarmonicNumber(number);
 	}
 
 }
--- BasicCorePrograms/src/com/utility/Utility.java
+++ BasicCorePrograms/src/com/utility/Utility.java
@@ -7,6 +7,23 @@ public class Utility {
 	Scanner scanner;
 
 	public Utility() {
+
+		scanner = new Scanner(System.in);
+
 	}
 
+	public int getIntValue() {
+		return scanner.nextInt();
+
+	}
+
+	public void getHarmonicNumber(int number) {
+		double harmonicNumber = 0;
+		for (double i = 1; i <= number; i++)
+
+		{
+			harmonicNumber = harmonicNumber + (1 / i);
+		}
+		System.out.println("n'th Harmonic Number of " + number + " is: " + harmonicNumber);
+	}
 }
